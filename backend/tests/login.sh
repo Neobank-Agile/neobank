4443c7e2e0d8927a894bd8aacefbf7b886b115ff
Christian Kuroki <ckuroki@gmail.com>
2023-01-20 22:19:40 -0300

    more testcases

--- backend/tests/login.sh
+++ backend/tests/login.sh
@@ -1,2 +1,9 @@
+# valid token
 curl -H 'Content-Type: application/json' -d '{"email": "jdoe@gmail.com","password": "supersecret"}' http://localhost:3001/login
+echo ""
+# invalid logins
+curl -H 'Content-Type: application/json' -d '{"email": "jdoe@gmail.com","password": "mypassword"}' http://localhost:3001/login
+echo ""
+curl -H 'Content-Type: application/json' -d '{"email": "johnDoe@gmail.com","password": "supersecret"}' http://localhost:3001/login
+echo ""
 
